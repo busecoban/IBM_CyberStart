8500789e1ea5b72b2cc0ee199f82df3e7aed4ca9
Buse Coban <buseccoban@gmail.com>
2024-05-11 04:29:38 +0300

    Update ContainerWithMostWater.py

--- ContainerWithMostWater.py
+++ ContainerWithMostWater.py
@@ -6,16 +6,10 @@ class Solution:
         
         while left < right:
             width = right - left
-            
-         
             h = min(height[left], height[right])
-            
-        
             water = width * h
-            
-           
             maxWater = max(maxWater, water)
-            
+
             if height[left] < height[right]:
                 left += 1
             else:
