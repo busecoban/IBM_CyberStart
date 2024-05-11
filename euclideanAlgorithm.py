1d88adc670845fd73d7e3367519971b9cdc1588e
Buse Coban <buseccoban@gmail.com>
2024-05-11 03:01:28 +0300

    Create euclideanAlgorithm.py

---
+++ euclideanAlgorithm.py
@@ -0,0 +1,17 @@
+import math
+
+points = [(0, 0), (3, 0), (3, 4), (3, 5)]
+def euclideanDistance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+distances = []
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+min_distance = min(distances)
+
+print("Min distance:", min_distance)
