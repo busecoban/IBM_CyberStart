f45cff9a613ec1610a763f07a081145ab0fb4574
Buse Coban <buseccoban@gmail.com>
2024-05-24 05:16:46 +0300

    Create quick_sort.py

---
+++ quick_sort.py
@@ -0,0 +1,11 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+arr = [5, 3, 6, 2, 10]
+print(quick_sort(arr))
