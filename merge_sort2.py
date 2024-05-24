b781f6eb700d686ea270d53c5c2b39240336bb3a
Buse Coban <buseccoban@gmail.com>
2024-05-24 05:20:05 +0300

    Create merge_sort2.py

---
+++ merge_sort2.py
@@ -0,0 +1,31 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+arr = [5, 3, 6, 2, 10]
+print(merge_sort(arr))
