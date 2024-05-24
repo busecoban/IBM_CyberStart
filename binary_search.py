fdbfc6b706502f81650bc71345dd2eca0265d053
Buse Coban <buseccoban@gmail.com>
2024-05-24 05:18:10 +0300

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,14 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+arr = [2, 3, 5, 6, 10]
+print(binary_search(arr, 6))  # Output: 3
