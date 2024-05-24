4e4e9c2bdd97c53bdadb0d89de6514527fd48cd6
Buse Coban <buseccoban@gmail.com>
2024-05-24 05:15:03 +0300

    Create SelectionSort.py

---
+++ SelectionSort.py
@@ -0,0 +1,12 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+arr = [5, 3, 6, 2, 10]
+print(selection_sort(arr))
